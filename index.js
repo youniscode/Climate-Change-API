7e713982fb25415352b541d6641e7709de7cf6be
Younis <youniscode@gmail.com>
2021-12-06 15:37:43 +0100

    addedTheNewsPathAndHowToGetNewspapersById

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 // Defining the PORT
 const PORT = 8000
-
 // Initialize Express
 const express = require('express')
-
-
 const axios = require('axios')
 const cheerio = require('cheerio')
-const { response } = require('express')
-
 // Calling express function and put the new express application inside the app variable to set new express application
 const app = express()
+
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change',
+    base: ''
+  },
+  {
+    name: 'guardian',
+    address: 'https://www.theguardian.com/environment/climate-crisis',
+    base: ''
+  },
+  {
+    name: 'telegraph',
+    address: 'https://www.telegraph.co.uk/climate-change/',
+    base: 'https://telegraph.co.uk'
+  }
+
+
+
+]
+// Create an empty array to return the contains inside as json format
+const articles = []
+
+newspapers.forEach(newspapers => {
+  axios.get(newspapers.address)
+    .then(response => {
+      const html = response.data
+      const $ = cheerio.load(html)
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text
+        const url = $(this).attr('href')
+
+        articles.push({
+          title,
+          url: newspapers.base + url,
+          source: newspapers.name
+        })
+      })
+
+    })
+})
 
 // home page route
 app.get('/', (req, res) => {
@@ -19,22 +57,12 @@ app.get('/', (req, res) => {
 
 // Get the path news as an HTML respond format (using axios)
 app.get('/news', (req, res) => {
-  axios.get('https://www.theguardian.com')
-    .then((response) => {
-      const html = response.data
-      // pick out the elements using cheerio
-      const $ = cheerio.load(html)
-      // look for any a tag that contains the word Climate
-      $('a:contains("climate")', html).each(function () {
-        const title = $(this).text()
-        const url = $(this).attr('href')
-        articles.push({
-          title,
-          url
-        })
-      })
-      res.json(articles)
-    }).catch((err) => console.log(err))
+  res.json(articles)
+})
+
+// route to get news from one newspaper article
+app.get('/news/:newspaperId', async (req, res) => {
+  const newspaperID = res.params.newspaperId
 })
 
 
